65e6f9d5c72c852c9ae3ddfdb996b7a634ae483b
Glitch752 <xxGlitch752xx@gmail.com>
2022-04-09 20:33:28 -0500

    Remove comments

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -19,20 +19,6 @@ export default function Editor() {
     const projects = store.get('projects', false);
     let { id } = useParams();
 
-    //Probably a way to optimize this, but it works for now
-    // let [selectionLoaded, loadSelection] = React.useState(false);
-    // let [selections, setSelections] = React.useState([]);
-    // let [editorCode, setEditorCode] = React.useState("");
-    // let [editorLanguage, setEditorLanguage] = React.useState("");
-    // let [selectedTab, setSelectedTab] = React.useState(0);
-    // let [deleteConfirm, setDeleteConfirm] = React.useState(false);
-    // //This works, but the entries are added many times. A ton of this code runs many times over.
-    // let [poppedOutSeletions, setPoppedOutSeletions] = React.useState([]);
-    // let [JSInjected, setJSInjected] = React.useState(false);
-    // let [editorChanges, setEditorChanges] = React.useState([]);
-    // let [programaticChangeMade, setProgramaticChangeMade] = React.useState(false);
-    // All of this, but a state machine now.
-
     const stateMachine = createMachine({
       id: "editor",
       initial: "loading",
@@ -160,23 +146,6 @@ export default function Editor() {
     // Remove all IPC listeners when the component is mounted again.
     ipc.removeAllListeners();
     
-    // if(selectionLoaded === false) {
-    //   ipc.send('getFiles', {directory: projects[id].directory}); // Send a message getFiles with the directpry of the project
-    // }
-
-    // ipc.once('getFilesReply', (event, args) => {
-    //   var loadingSelections = [];
-    //   var editorChangesTemp = [];
-    //   for(let i = 0; i < args.files.length; i++) {
-    //     loadingSelections.push(args.files[i]);
-    //     editorChangesTemp.push(false);
-    //   }
-    //   loadSelection(true);
-    //   setSelections(loadingSelections);
-    //   setEditorChanges(editorChangesTemp);
-    //   ipc.send('getFile', {file: path.join(args.directory, args.files[0])});
-    // });
-    
     ipc.once('getFilesReply', async (event, args) => {
       var loadingSelections = [];
       for(let i = 0; i < args.files.length; i++) {
@@ -194,28 +163,6 @@ export default function Editor() {
       if(state.can('finishedLoading')) send("finishedLoading");
       ipc.send('getFile', {file: path.join(args.directory, args.files[0])});
     });
-
-    // let selectTab = (tab) => {
-    //   setSelectedTab(tab);
-    //   if(tab >= 0) {
-    //     ipc.send('getFile', {file: path.join(projects[id].directory, selections[tab])});
-    //   }
-    // }
-
-    // ipc.once('getFileReply', (event, args) => {
-    //   if(args.fileName !== undefined) {
-    //     var extToLang = { // List of compatible files with Monaco language support
-    //       ".html": "html",
-    //       ".css": "css",
-    //       ".js": "javascript",
-    //       ".svg": "html",
-    //     };
-    //     setProgramaticChangeMade(true);
-    //     setEditorLanguage(extToLang[path.extname(args.fileName)]);
-    //   }
-    //   setProgramaticChangeMade(true);
-    //   setEditorCode(args.content);
-    // });
 
     ipc.once('getFileReply', async (event, args) => {
       while(editor === null) {
@@ -239,182 +186,6 @@ export default function Editor() {
       }
       editor.setValue(args.content);
     });
-
-    // let popOut = () => {
-    //   ipc.send('editorPopOut', {file: path.join(projects[id].directory, selections[selectedTab]), index: selectedTab});
-    // }
-
-    // var poppedOutSelectionsIndexes = [];
-    // var poppedOutSelectionsWindows = [];
-
-    // let updatePoppedOut = () => {
-    //   poppedOutSelectionsIndexes = poppedOutSeletions.map((selection) => {return selection.index;});
-    //   poppedOutSelectionsWindows = poppedOutSeletions.map((selection) => {return selection.window;});
-    // };
-
-    // updatePoppedOut();
-
-    // ipc.once('editorPopoutReply', (event, args) => {
-    //   var poppedOutSelectionsTemp = poppedOutSeletions;
-    //   poppedOutSelectionsTemp.push(args);
-    //   setPoppedOutSeletions(poppedOutSelectionsTemp);
-    //   updatePoppedOut();
-    //   for(let i = 0; i < selections.length; i++) {
-    //     if(poppedOutSelectionsIndexes.includes(i)) {
-    //       continue;
-    //     } else {
-    //       selectTab(i);
-    //       return;
-    //     }
-    //   }
-    // });
-
-    // ipc.once('popoutClose', (event, args) => {
-    //   setPoppedOutSeletions(poppedOutSeletions.filter(x => x.index !== args));
-    //   console.log(poppedOutSeletions);
-    // });
-
-    // let focusWindow = (window) => {
-    //   ipc.send('editorFocusWindow', window);
-    // }
-
-    // var selectMenus = [];
-
-    // for(let i = 0; i < selections.length; i++) {
-    //   let isSelected = (selectedTab === i ? styles.selectedSelection : "");
-    //   if(poppedOutSelectionsIndexes.includes(i)) {
-    //     //Add different popped out selection and make it so you can't select it
-    //     selectMenus.push(
-    //       <div key={i} className={styles.editorSelection + " " + isSelected} onClick={() => focusWindow(poppedOutSelectionsWindows[poppedOutSelectionsIndexes.indexOf(i)])}><i className={"fas fa-arrow-up-right-from-square " + styles.editorSelectionIcon}></i>{selections[i]}</div>
-    //     );
-    //   } else {
-    //     let IsSavedElement = editorChanges[i] ? <i className={"fas fa-save " + styles.editorSelectionIcon}></i> : "";
-    //     selectMenus.push(
-    //       <div key={i} className={styles.editorSelection + " " + isSelected} onClick={() => selectTab(i)}><i className={"fas fa-angle-right " + styles.editorSelectionIcon}></i>{selections[i]}{IsSavedElement}</div>
-    //     );
-    //   }
-    // }
-
-    // let deleteProjectConfirm = () => {
-    //   var currentProjects = store.get('projects', []);
-    //   ipc.send('deleteProject', {directory: currentProjects[id].directory});
-    //   currentProjects.splice(id, 1);
-    //   store.set('projects', currentProjects);
-    // }
-
-    // ipc.once('deleteProjectReply', (event, args) => {
-    //   if(args === true) {
-    //     navigate('/');
-    //   } else {
-    //     navigate('/Error', {state: {error: "Error deleting project!", errorMessage: args}});
-    //   }
-    // });
-
-    // let deleteProject = () => {
-    //   setDeleteConfirm(true);
-    // }
-
-    // // let updateLayoutEditor = () => {
-    // //   ipc.send('getLayoutEditorHTML', {directory: projects[id].directory, index: "index.html"});
-    // // }
-
-    // // updateLayoutEditor();
-
-    // // ipc.on('getLayoutEditorHTMLReply', (event, args) => {
-    // // });
-
-    // let deleteProjectElement = null;
-
-    // let InjectJS = () => {
-    //     ipc.send('getAppPath');
-    // }
-
-    // if(!JSInjected) {
-    //   setJSInjected(true);
-    //   ipc.on('getAppPathReply', (event, args) => {
-    //       var iFrameHead = window.frames["editorFrame"].document.getElementsByTagName("head")[0];
-    //       var myscript = document.createElement('script');
-    //       myscript.type = 'text/javascript';
-    //       myscript.src = path.join(args, '/pages/editorLayoutInjectScript.js');
-    //       iFrameHead.appendChild(myscript);
-    //   });
-    // }
-
-    // if(deleteConfirm) {
-    //   deleteProjectElement =
-    //     <div className={styles.confirmDelete}>
-    //       <div className={styles.confirmDeleteContainer}>
-    //         <div className={styles.confirmDeleteText}>Are you sure you want to delete this project? This action is irreversible.</div>
-    //         <div className={styles.confirmDeleteButtons}>
-    //           <button className={`${styles.confirmDeleteButton} ${styles.confirmDeleteButtonCancel}`} onClick={() => setDeleteConfirm(false)}>Cancel</button>
-    //           <button className={styles.confirmDeleteButton} onClick={() => deleteProjectConfirm()}>Delete</button>
-    //         </div>
-    //       </div>
-    //     </div>
-    // } else {
-    //   deleteProjectElement = <></>;
-    // }
-
-    // const settignsSelected = (selectedTab === -1 ? styles.selectedSelection : "");
-    // const layoutEditorSelected = (selectedTab === -2 ? styles.selectedSelection : "");
-    // const defaultSelections = [
-    //   {
-    //     element: [
-    //       <div key="settings" className={styles.editorSelection + " " + layoutEditorSelected} onClick={() => selectTab(-2)}>
-    //         <i className={"fas fa-table " + styles.editorSelectionIcon}></i>
-    //         Layout editor
-    //       </div>
-    //     ],
-    //     menu: [
-    //       // Add layout editor: render the files in the project directory.
-    //       <div key="layout" className={styles.layoutEditor}> 
-    //         <div className={styles.layoutEditorPage}>
-    //           <iframe src={`file://${projects[id].directory}/index.html`} className={styles.projectIFrame} name="editorFrame" id="editorFrame" onLoad={() => InjectJS()}></iframe>
-    //         </div>
-    //       </div>
-    //     ],
-    //     id: -2,
-    //     name: "Layout editor",
-    //   },
-    //   {
-    //     element: [
-    //       <div key="settings" className={styles.editorSelection + " " + settignsSelected} onClick={() => selectTab(-1)}>
-    //         <i className={"fas fa-gear " + styles.editorSelectionIcon}></i>
-    //         Settings
-    //       </div>
-    //     ],
-    //     menu: [
-    //       <div key="settings" className={styles.settingsMenu}>
-    //         <div className={styles.settingsMenuSeparator}>Misc</div>
-    //         <div className={styles.settingsMenuSection}>
-    //           Some sort of settings menu idk
-    //         </div>
-    //         <div className={`${styles.settingsMenuSeparator} ${styles.settingMenuDanger}`}>DANGER ZONE</div>
-    //         <div className={styles.settingsMenuSection}>
-    //           <button className={styles.deleteProjectButton} onClick={() => deleteProject()}>Delete Project</button>
-    //         </div>
-    //       </div>
-    //     ],
-    //     id: -1,
-    //     name: "Settings",
-    //   },
-    // ];
-
-    // const defaultSelectionsElements = defaultSelections.map((selection) => {return selection.element;});
-    // const finalSelections = defaultSelectionsElements.concat(selectMenus);
-
-    // let editingMenu = [<div key="loading">Loading</div>];
-    // let editorName = "Loading";
-
-    // if(selectedTab >= 0) {
-    //   editingMenu = editorPane;
-    //   editorName = "Code editor - " + selections[selectedTab];
-    // } else {
-    //   //Find the element in defaultSelections with the id of selectedTab
-    //   let selectionElement = defaultSelections.filter(x => x.id === selectedTab)[0];
-    //   editingMenu = selectionElement.menu;
-    //   editorName = selectionElement.name;
-    // }
     
     let editorTabs = state.context.editorTabs;
     let editorTab = state.context.tab;

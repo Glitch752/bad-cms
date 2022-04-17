7643e9d9ade4cfc9542e049a2fa033ca31299372
Glitch752 <xxGlitch752xx@gmail.com>
2022-04-17 16:50:14 -0500

    Remove old files

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { App } from './App';
-import { remote } from 'electron';
-
-//import LogRocket from 'logrocket';
-//LogRocket.init('b8unfg/badcmsfordevs');
-
-render(<App />, document.getElementById('root'));
